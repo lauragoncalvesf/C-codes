6cc797d1fe14c3d590df6c7afc0262ea0e1f641d
Laura Gonçalves <lauragoncalvesf804@outlook.com>
2024-03-18 15:33:32 -0300

    vai tomar nocu

--- .c
+++ .c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
+    int a,b;
+    int soma;
 
+
+    scanf("%d %d",&a, &b );
+    soma = a+b;
+    printf("soma: %d\n", soma);
+
+    printf("Hello World!\n");
 }
-
